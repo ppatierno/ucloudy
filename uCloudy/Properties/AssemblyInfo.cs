5145c178003dd0d21d938bf7c8c9819d1670a87f
ppatierno <ppatierno@live.com>
2015-08-14 13:34:11 +0200

    First commit on GitHub moved from CodePlex

---
+++ uCloudy/Properties/AssemblyInfo.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("uCloudy")]
+[assembly: AssemblyDescription("uCloudy is a library for .NET Micro Framework that provides a set of clients to connect to the main services in the cloud.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Paolo Patierno")]
+[assembly: AssemblyProduct("uCloudy")]
+[assembly: AssemblyCopyright("Copyright © Paolo Patierno 2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
